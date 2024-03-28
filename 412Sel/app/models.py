ef29aeff7951b44abb5339918688d0a66ac8d574
SpyFoxGold <125376504+SpyFoxGold@users.noreply.github.com>
2024-03-28 15:47:54 +0700

    Add files via upload
    Исправили и заработало немного
    Задачи: добавление картинки post

---
+++ 412Sel/app/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "Users"
+    id: int = Column(Integer, primary_key=True, index=True)
+    username: str = Column(String(50), index=True)
+    request: str = Column(String(50), unique=True, index=True)
